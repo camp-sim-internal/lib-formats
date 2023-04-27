fd8e545071b1e7fe9e9ef72aeece7a14280e8f6a
JoaoWudarski <joaowudarski@gmail.com>
2023-04-27 10:34:20 -0300

    Adicionar método padrão ao GameSimulator

--- src/main/java/br/com/campsim/game/GameSimulator.java
+++ src/main/java/br/com/campsim/game/GameSimulator.java
@@ -7,7 +7,16 @@ import br.com.campsim.domain.Team;
 public interface GameSimulator<T> {
 
     Result simulate(Team<T> teamA, Team<T> teamB, boolean simulateWithDrawCase, boolean printGameHistoric);
-    ResultList simulate(Team<T> teamA, Team<T> teamB, int bestOf, boolean simulateWithDrawCase, boolean printGameHistoric);
 
     Result simulateDrawCase(Team<T> teamA, Team<T> teamB, boolean printGameHistoric);
+
+    default ResultList simulate(Team<T> teamA, Team<T> teamB, int bestOf, boolean simulateWithDrawCase, boolean printGameHistoric) {
+        ResultList resultList = new ResultList(bestOf);
+
+        while(resultList.isNotOver()) {
+            resultList.addResult(this.simulate(teamA, teamB, simulateWithDrawCase, printGameHistoric));
+        }
+
+        return resultList;
+    }
 }
